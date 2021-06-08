e11d9711879949917bca16134e2bf4237fbbf8cc
Eston Kagwima <ekagwima745@gmail.com>
2021-06-08 23:37:14 +0300

    creating registerapi view for user

--- lightsave/views.py
+++ lightsave/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+
+from rest_framework.views import APIView
+
 
 # Create your views here.
+class RegisterApiView(APIView):
+  def post(self, request):
+    pass
+
