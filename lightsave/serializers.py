d30bd1af5fdd34377a4224c713691f3881ebba7e
Eston Kagwima <ekagwima745@gmail.com>
2021-06-08 23:57:59 +0300

    adding register view

--- lightsave/serializers.py
+++ lightsave/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
    class Meta:
         model = User
         fields = [ 'id', 'username','email','password']
-
+        extra_kwargs = {'password': {'write_only': True}}
         def create(self, validated_data):
           password =validated_data.pop('password', None)
           instance =self.Meta.Model(**validated_data)
--- lightsave/views.py
+++ lightsave/views.py
@@ -1,3 +1,17 @@
 
+from rest_framework import serializers
+from lightsave.serializers import UserSerializer
+from rest_framework.serializers import Serializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+# Create your views here.
+class RegisterApiView(APIView):
+  def post(self, request):
+    serializer = UserSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data)
 
 
