2a4f68a8e06c2eb8e4269a67fb9a220f32e15427
Eston Kagwima <ekagwima745@gmail.com>
2021-06-08 23:55:58 +0300

    create user model serializer

--- lightsave/models.py
+++ lightsave/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-  name = models.CharField(max_length=255)
+  name = models.CharField(max_length=255, null=True)
   email = models.CharField(max_length=255, unique=True)
   password =models.CharField(max_length=255)
   
