5199eb42ec10daa32e7e5bea92dae92d18984854
Eston Kagwima <ekagwima745@gmail.com>
2021-06-09 22:20:24 +0300

    adding new models to admin page

--- lightsave/admin.py
+++ lightsave/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import User,Appliance,CustomAppliance
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Appliance)
+admin.site.register(CustomAppliance)
